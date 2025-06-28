1ba4f6f39b0dffb85c6d8b369421d5741b726594
Charles Barilleaux <chbarr@bandman.local>
2025-06-28 11:50:30 -0400

    Playing with Tuples

--- rust/data-types/src/main.rs
+++ rust/data-types/src/main.rs
@@ -1,7 +1,8 @@
 fn main() {
-    println!("\n\n\tThe Rust Programming Language Chapter 3, Section 2: Data Types\n");
+    println!("\n\n\t# The Rust Programming Language Chapter 3, Section 2: Data Types\n");
     
-    println!("INTEGERS");
+    println!("## Scaler Types");
+    println!("### INTEGERS");
     let mut foo: u32=50; //32-bit unsigned integer
     println!("Foo={foo}");
     foo=foo*foo;
@@ -9,11 +10,11 @@ fn main() {
     let mut bar: u32=0xffff;
     println!("Bar={bar}\tI put this in as a hex number (0xffff). It printed as decimal.");
 
-    println!("\nFLOATING TYPES");
+    println!("\n### FLOATING TYPES");
     let mut baz: f32=3.14;
     println!("Baz={baz}\tNot rocket science.");
 
-    println!("\nLET'S DO SOME MATH!");
+    println!("\n### LET'S DO SOME MATH!");
     let truncated=5/3;
     println!("5/3={truncated}\tIt assumed integer");
     let floatingtruncated=5.0/3.0;
@@ -22,12 +23,22 @@ fn main() {
     let mixnumbers=rock as f64 +floatingtruncated; //"as" is key.
     println!("Mixing numbers gives me {mixnumbers}");
 
-    println!("\nBOOLEAN\nJust setting a couple in the code. It's pretty straight-forward.");
+    println!("\n### BOOLEAN\nJust setting a couple in the code. It's pretty straight-forward.");
     let t=true;
     let f: bool=false;
 
-    println!("\nCHARACTER");
+    println!("\n### CHARACTER");
     let c='c';
     println!("The caracter is \"{c}\"");
 
+    println!("\n## Compound Types");
+    println!("\n### Tuple Type");
+    let trey: (i32,f64,u8)=(500,24.8,3);
+    println!("Accessing the first element: {0}",trey.0);
+    println!("Accessing the third element: {0}",trey.2);
+    println!("Accessing the second element: {0}",trey.1);
+    println!("Can a tuple have more than three elements?");
+    let quint: (i32,i32,i32,i32,i32)=(10,20,30,40,50);
+    println!("Fifth Element: {0}",quint.4);
+    println!("Yep!");
 }
